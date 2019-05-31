04dbd8333d38769664161fe4ff5f6479b31f7d86
dackng <dackng@gmail.com>
2019-05-31 17:52:15 -0500

    removing a simple comment

--- src/main/java/util/PropertiesValue.java
+++ src/main/java/util/PropertiesValue.java
@@ -9,7 +9,7 @@ public class PropertiesValue {
 	public static final String DB_USERNAME = PropertiesConfig.getString("db.username");
 	public static final String DB_PASSWORD = PropertiesConfig.getString("db.password");
 	
-	//#file directory
+	//file directory
 	public static final String FILE_DIRECTORY = PropertiesConfig.getString("file.directory");
 	public static final String FILE_NAME = PropertiesConfig.getString("file.name");
 }
